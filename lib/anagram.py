a49fbe46d68fc94fd37e3a1e8e0429342298535c
Vkaisang41 <kaisang.vincent@student.moringaschool.com>
2025-08-21 10:17:59 +0300

    Completed Anagram Detector Lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word: str):
+        self.word = word
+        self._norm = word.lower()
+        self._signature = tuple(sorted(self._norm))
+
+    def match(self, candidates):
+        matches = []
+        for cand in candidates:
+            c_norm = cand.lower()
+            if c_norm == self._norm:
+                continue
+            if tuple(sorted(c_norm)) == self._signature:
+                matches.append(cand)
+        return matches
+
